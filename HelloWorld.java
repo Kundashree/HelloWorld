171c5e28a9100b48875b25bfddc1e34fadf39c87
Kundashre A <kuna@publicisgroupe.net>
2022-03-30 14:15:24 +0530

    code modify

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld
  {
 	public static void main (String[] args) {
 		System.out.println("hello world!");
+		System.out.println(100*34);
+		System.out.println("********************");
 		}
 }
